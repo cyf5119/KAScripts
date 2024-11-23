6fa109e8b3c3126cbaaaf8462dd3ebb4ca3bb470
cyf5119 <89454904+cyf5119@users.noreply.github.com>
2024-11-23 19:40:09 +0800

    adjust aal bomb

--- 06-Endwalker/AalBomb.cs
+++ 06-Endwalker/AalBomb.cs
@@ -15,16 +15,21 @@ using KodakkuAssist.Module.Draw;
 
 namespace Cyf5119Script.Endwalker.AalBomb;
 
-[ScriptType(guid: "2F27EFB2-CE87-4E6A-9D83-60FE4CC26141", name: "AalBomb", territorys: [1179, 1180], version: "0.0.0.1", author: "Cyf5119")]
+[ScriptType(guid: "2F27EFB2-CE87-4E6A-9D83-60FE4CC26141", name: "AalBomb", territorys: [1179, 1180], version: "0.0.0.2", author: "Cyf5119")]
 public class AalBomb
 {
     [UserSetting(note:"每轮炸弹检测间隔（毫秒）")]
     public int Prop1 { get; set; } = 100;
     
+    [UserSetting(note:"炸弹检测轮数上限")]
+    public int Prop2 { get; set; } = 300;
+    
+    public int Is2nd = 0;
     public List<IBattleChara> bombs = new List<IBattleChara>();
     
     public void Init(ScriptAccessory accessory)
     {
+        Is2nd = 0;
         bombs.Clear();
     }
     
@@ -43,9 +48,21 @@ public class AalBomb
             return false;
         }
     }
+
+    [ScriptMethod(EventTypeEnum.StartCasting, "Aero", ["ActionId:regex:^(35145|35174)$"], false)]
+    public void Aero(Event @event, ScriptAccessory accessory)
+    {
+        Is2nd = 0;
+    }
+    
+    [ScriptMethod(EventTypeEnum.StartCasting, "RingARingOExplosions", ["ActionId:regex:^(35164|35193)$"], false)]
+    public void RingARingOExplosions(Event @event, ScriptAccessory accessory)
+    {
+        Is2nd++;
+    }
     
     [ScriptMethod(name: "生成炸弹", eventType: EventTypeEnum.AddCombatant, eventCondition: ["DataId:regex:^(1648[19])$"])]
-    public unsafe void BombSpawn(Event @event, ScriptAccessory accessory)
+    public void BombSpawn(Event @event, ScriptAccessory accessory)
     {
         if (!ParseObjectId(@event["SourceId"], out var sid)) return;
         var source = (IBattleChara?)accessory.Data.Objects.SearchById(sid);
@@ -73,25 +90,28 @@ public class AalBomb
 
     public unsafe void BombDetect(ScriptAccessory accessory)
     {
-        if (bombs.Count < 6) return;
-        foreach (var bomb in bombs)
+        for (int i = 0; i < Prop2; i++)
         {
-            if (bomb.Struct()->Timeline.AnimationState[0] > 0)
+            if (bombs.Count < 6) return;
+            foreach (var bomb in bombs)
             {
-                // accessory.Method.SendChat($"/e 已找到 {bomb.EntityId:X}");
-                DrawBomb(accessory, bomb);
-                return;
+                if (bomb.Struct()->Timeline.AnimationState[0] > 0)
+                {
+                    // accessory.Method.SendChat($"/e 已找到 {bomb.EntityId:X}");
+                    DrawBomb(accessory, bomb);
+                    return;
+                }
             }
+            Thread.Sleep(Prop1);
         }
-        Thread.Sleep(Prop1);
-        BombDetect(accessory);
+        // BombDetect(accessory);
     }
 
     public unsafe void DrawBomb(ScriptAccessory accessory, IBattleChara bomb0)
     {
         var dp = accessory.Data.GetDefaultDrawProperties();
         dp.Name = "老三炸弹";
-        dp.Color = accessory.Data.DefaultDangerColor;
+        dp.Color = Is2nd == 2 ? accessory.Data.DefaultDangerColor.WithW(4) : accessory.Data.DefaultDangerColor;
         dp.DestoryAt = 21000;
         dp.Scale = new Vector2(12);
         dp.Owner = bomb0.EntityId;
